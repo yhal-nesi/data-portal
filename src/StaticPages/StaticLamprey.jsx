025c6c234dfd2a4b1ffb92c84a9a67b6d70e9735
jhuh003 <jhuh003@aucklad.ac.nz>
2021-08-11 21:49:30 +1200

    Static pages

---
+++ src/StaticPages/StaticLamprey.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { components } from '../params';
+
+class StaticLamprey extends React.Component {
+  render() {
+    return (
+      <div class="protected-content "><div class="index-page"><div class="index-page__top"><div class="prose field field-name-body kakapo"><h1>Slippery when wet: cross-species transmission of divergent coronaviruses in bony and jawless fish and the evolutionary history of the Coronaviridae
+      </h1><p></p>Virus discovery in New Zealand lamprey (kanakana).
+      <p></p><p><b>Number of files: </b>6</p>
+      <p><b>Date collected: </b>2012</p>
+      <p><b>Support source: </b>Rutherford Discovery Fellowship; Marsden Fund
+      </p><p><b>Primary investigator: </b><a href="mailto:jemma.geoghegan@otago.ac.nz">Jemma Geoghegan</a>, University of Otago</p><h4>How to access the data</h4><b><a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScerXN4MLIAgnLfrHbvheMwtM6gtDNaluZ3c_-xEiot2hc6DA/viewform?usp=pp_url&entry.129341974=Lamprey:+Virus+discovery+in+New+Zealand+lamprey+(kanakana)">Apply for access</a></b><br/></div></div></div></div>
+    );
+  }
+}
+
+export default StaticLamprey;
