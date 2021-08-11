56fed93ce21c7fab3033d2419d57265ae6852a1a
jhuh003 <jhuh003@aucklad.ac.nz>
2021-08-11 15:31:53 +1200

    Genomics -> Genomic, About page link changes

--- src/StaticPages/StaticAbout.jsx
+++ src/StaticPages/StaticAbout.jsx
@@ -7,17 +7,10 @@ class StaticAbout extends React.Component {
       <div class="protected-content "><div class="index-page"><div class="index-page__top">
       <div class="prose field field-name-body about"><h1>About</h1><p>
       </p>
-      The Aotearoa Genomic Data Repository (AGDR) is an online resource for the storage and sharing of digital genomic data. The AGDR is being developed by Genomics Aotearoa and the New Zealand eScience Infrastructure (NeSI), as part of a programme of research funded by the NZ Ministry for Business Innovation and Employment.
+      The Aotearoa Genomic Data Repository (AGDR) is an online resource for the storage and sharing of digital genomic data. The AGDR is being developed by <a target="_blank" href="https://www.genomics-aotearoa.org.nz/">Genomics Aotearoa</a> and the <a target="_blank" href="https://www.nesi.org.nz/">New Zealand eScience Infrastructure (NeSI)</a>, as part of a programme of research funded by the NZ Ministry for Business Innovation and Employment.
       <br/>
       <br/>
-      The AGDR follows the principles of Māori Data Sovereignty, and facilitates data submission and access through a process that involves close interaction with those who hold kaitiaki over each data set. By default, data are not publicly available, but requests for access to specific data sets can be made via the online form on each of the <a href="/study-viewer/project">project pages</a>.
-      <br/>
-      <br/>
-      Link for Genomics Aotearoa: <a href="https://www.genomics-aotearoa.org.nz/">https://www.genomics-aotearoa.org.nz/</a>
-      <br/>
-      Link for New Zealand eScience Infrastructure: <a href="https://www.genomics-aotearoa.org.nz/">https://www.genomics-aotearoa.org.nz/</a>
-      <br/>
-      Link for Māori Data Sovereignty: <a href="https://www.temanararaunga.maori.nz/">https://www.temanararaunga.maori.nz/</a>
+      The AGDR follows the principles of <a target="_blank" href="https://www.temanararaunga.maori.nz/">Māori Data Sovereignty</a>, and facilitates data submission and access through a process that involves close interaction with those who hold kaitiaki over each data set. By default, data are not publicly available, but requests for access to specific data sets can be made via the online form on each of the <a href="/study-viewer/project">project pages</a>.
       <br/></div>
       </div></div></div>
     );
